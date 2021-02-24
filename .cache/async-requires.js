2e305efa1756a96d214654218d4d065528a663ce
Rishabh Bisht <ribisht@oradev.oraclecorp.com>
2021-02-24 21:31:14 +0530

    change tailwind

---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
+  "component---src-pages-articles-tsx": () => import("./../../../src/pages/articles.tsx" /* webpackChunkName: "component---src-pages-articles-tsx" */),
+  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */),
+  "component---src-pages-products-tsx": () => import("./../../../src/pages/products.tsx" /* webpackChunkName: "component---src-pages-products-tsx" */),
+  "component---src-pages-sociallyacclaimed-tsx": () => import("./../../../src/pages/sociallyacclaimed.tsx" /* webpackChunkName: "component---src-pages-sociallyacclaimed-tsx" */),
+  "component---src-pages-trending-tsx": () => import("./../../../src/pages/trending.tsx" /* webpackChunkName: "component---src-pages-trending-tsx" */),
+  "component---src-templates-article-template-tsx": () => import("./../../../src/templates/articleTemplate.tsx" /* webpackChunkName: "component---src-templates-article-template-tsx" */),
+  "component---src-templates-product-template-tsx": () => import("./../../../src/templates/productTemplate.tsx" /* webpackChunkName: "component---src-templates-product-template-tsx" */)
+}
+
